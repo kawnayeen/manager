a7ea2d270cdcf2bae709dd782ac25e97e9469954
kamarul kawnayeen <kawnayeen.08@gmail.com>
2017-04-19 23:23:50 +0600

    Switching back between 'EmployeeList' & 'EmployeeCreate'

---
+++ src/components/employee.create.js
@@ -0,0 +1,17 @@
+/**
+ * Created by kawnayeen on 4/19/17.
+ */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeCreate extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Create New Employee </Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeCreate;
--- src/router.js
+++ src/router.js
@@ -2,9 +2,10 @@
  * Created by kawnayeen on 4/19/17.
  */
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/login.form';
 import EmployeeList from './components/employee.list';
+import EmployeeCreate from './components/employee.create';
 
 const RouterComponent = () => (
     <Router sceneStyle={{ paddingTop: 65 }}>
@@ -12,7 +13,19 @@ const RouterComponent = () => (
             <Scene key="login" component={LoginForm} title="Please Login" />
         </Scene>
         <Scene key="main">
-            <Scene key="employeeList" component={EmployeeList} title="Employees" />
+            <Scene
+                onRight={() => Actions.employeeCreate()}
+                rightTitle="Add"
+                key="employeeList"
+                component={EmployeeList}
+                title="Employees"
+                initial
+            />
+            <Scene
+                key="employeeCreate"
+                component={EmployeeCreate}
+                title="Create Employee"
+            />
         </Scene>
     </Router>
 );
