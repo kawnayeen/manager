6e27a20ec84c357ad47ded1d13aacbb0205516a6
kawnayeen <kawnayeen.08@gmail.com>
2017-04-21 21:56:26 +0600

    loadash dependency added & used to map the single object from redux to list of employees

--- src/components/employee.list.js
+++ src/components/employee.list.js
@@ -1,15 +1,30 @@
 /**
  * Created by kawnayeen on 4/19/17.
  */
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { employeeFetch } from '../actions';
 
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeeFetch();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        // nextProps are the next set of props that this component will be rendered with
+        // this.props is still the old set of props
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.dataSource = ds.cloneWithRows(employees);
     }
 
     render() {
@@ -21,4 +36,12 @@ class EmployeeList extends Component {
     }
 }
 
-export default connect(null, { employeeFetch })(EmployeeList);
+const mapStateToProp = (state) => {
+    const employees = _.map(state.employees, (val, uid) => {
+       return { ...val, uid };
+    });
+
+    return { employees };
+};
+
+export default connect(mapStateToProp, { employeeFetch })(EmployeeList);
--- src/reducer/EmployeeReducer.js
+++ src/reducer/EmployeeReducer.js
@@ -1,17 +1,14 @@
 /**
  * Created by kawnayeen on 4/21/17.
  */
-import {
-    EMPLOYEE_FETCH_SUCCESS
-} from '../actions/type';
+import { EMPLOYEE_FETCH_SUCCESS } from '../actions/type';
 
 const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMPLOYEE_FETCH_SUCCESS:
-            console.log(action);
-            return state;
+            return action.payload;
         default:
             return state;
     }
